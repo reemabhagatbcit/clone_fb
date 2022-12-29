748f274cc0069844621127f09a886b49c500d329
reema <reema@blackcapit.com>
2022-12-29 16:54:57 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+   
+    extend: {
+      colors: {
+        'green-custom':'#42b72a',
+        'green-hover':'#36a420',
+        'blue-custom':'#1877f2',
+        'gray-footer':'#737373',
+      }
+    
+    },
+  },
+  plugins: [],
+}
